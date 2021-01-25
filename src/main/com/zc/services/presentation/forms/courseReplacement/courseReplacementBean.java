4db443a2f12e9b459471b4a62215ad1320141937
Ahmed.Dakrory <ahmedsonic1993@gmail.com>
2021-01-25 10:54:52 +0200

    Solve issue regarding major type

--- src/main/com/zc/services/presentation/forms/courseReplacement/courseReplacementBean.java
+++ src/main/com/zc/services/presentation/forms/courseReplacement/courseReplacementBean.java
@@ -317,9 +317,9 @@ HttpServletRequest origRequest = (HttpServletRequest)FacesContext.getCurrentInst
 	public String getMajorType(int type) {
 		switch(type){
 		case 1:
-			return "Science";
-		case 2:
 			return "Engineering";
+		case 2:
+			return "Science";
 			
 		}
 		return "";
